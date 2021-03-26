8f486eacf16274fda2972422a3a60d8663c59e24
acronhub <shiratama.make@gmail.com>
2021-03-26 22:44:47 +0900

    ローカルとCloudRunで保存方法を変更

---
+++ app/config/env/production/plugins.js
@@ -0,0 +1,10 @@
+module.exports = ({ env }) => ({
+  upload: {
+    provider: 'google-cloud-storage',
+    providerOptions: {
+      bucketName: env('BUCKET_NAME', 'strapi-resources'),
+      baseUrl: 'https://storage.cloud.google.com/{bucket-name}',
+      uniform: true,
+    },
+  },
+});
