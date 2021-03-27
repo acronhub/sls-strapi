2d2ddd6237d7b81bf1f94353b0785c0e603ff412
acronhub <shiratama.make@gmail.com>
2021-03-28 01:33:09 +0900

    メディア先の修正

--- app/config/plugins.js
+++ app/config/plugins.js
@@ -1,10 +1,1 @@
-module.exports = ({ env }) => ({
-    upload: {
-      provider: 'google-cloud-storage',
-      providerOptions: {
-        bucketName: env('BUCKET_NAME', 'strapi-resources'),
-        baseUrl: 'https://storage.cloud.google.com/{bucket-name}',
-        uniform: true,
-      },
-    },
-  });
+module.exports = () => {};
